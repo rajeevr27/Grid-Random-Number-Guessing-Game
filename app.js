edc1ca685c05b571bfcd80ad9b65baf1c659f76a
rajeevr27 <39468702+rajeevr27@users.noreply.github.com>
2022-03-27 11:00:05 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,37 +18,37 @@ const restartGame = function() {
 for (let i = 0; i < gridItem.length; i += 1) {
   gridItem[i].addEventListener("click", function() {
       if (i == randomNumber) {
-      div = this;
-      indicatorMessage.textContent = `You Guessed Right! The Random Number is ${randomNumber+1}.`;
-      indicatorMessage.style.color = "green";
-      div.style.backgroundColor = "green";
-      div.style.color = "white";
-      disableGridItems();
+        div = this;
+        indicatorMessage.textContent = `You Guessed Right! The Random Number is ${randomNumber+1}.`;
+        indicatorMessage.style.color = "green";
+        div.style.backgroundColor = "green";
+        div.style.color = "white";
+        disableGridItems();
     } else if (previousGuesses.indexOf(i) > -1) {
-      gridItem[i].style.pointerEvents = "none"
+        gridItem[i].style.pointerEvents = "none"
     } else if (guessCount === 9) {
-      div = this;
-      indicatorMessage.textContent = `You have reached the maximum number of guesses allowed.`;
-      indicatorMessage.style.color = "red";
-      div.style.backgroundColor = "red";
-      div.style.color = "white";
-      disableGridItems();
+        div = this;
+        indicatorMessage.textContent = `You have reached the maximum number of guesses allowed.`;
+        indicatorMessage.style.color = "red";
+        div.style.backgroundColor = "red";
+        div.style.color = "white";
+        disableGridItems();
     } else if (i > randomNumber) {
-      div = this;
-      indicatorMessage.textContent = `Guess < Lower`;
-      indicatorMessage.style.color = "black";
-      div.style.backgroundColor = "red";
-      div.style.color = "white";
-      previousGuesses.push(i);
-      guessCount += 1;
+        div = this;
+        indicatorMessage.textContent = `Guess < Lower`;
+        indicatorMessage.style.color = "black";
+        div.style.backgroundColor = "red";
+        div.style.color = "white";
+        previousGuesses.push(i);
+        guessCount += 1;
     } else if (i < randomNumber) {
-      div = this;
-      indicatorMessage.textContent = `Guess > Higher`;
-      indicatorMessage.style.color = "black";
-      div.style.backgroundColor = "red";
-      div.style.color = "white";
-      previousGuesses.push(i);
-      guessCount += 1;
+        div = this;
+        indicatorMessage.textContent = `Guess > Higher`;
+        indicatorMessage.style.color = "black";
+        div.style.backgroundColor = "red";
+        div.style.color = "white";
+        previousGuesses.push(i);
+        guessCount += 1;
     }
  });//End of gridItem function
 }//End of for loop

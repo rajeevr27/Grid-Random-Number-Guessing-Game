2fe4f5009eba92810ff0d4f392e0f7a8f99c0a61
rajeevr27 <39468702+rajeevr27@users.noreply.github.com>
2022-04-10 12:51:27 -0700

    Changes to files

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const randomNumber = Math.floor(Math.random() * 25);//Generates random integer between 0 and 24
 const indicatorMessage = document.querySelector('h2');
+const guessHistory = document.querySelector('#message-container h3');
 const gridItem = document.getElementsByClassName("grid-item");
 const previousGuesses = [];
 let guessCount;
 let div;
+
+function guessListHistory() {
+  guessHistory.innerHTML = `<ul>`;
+    for (let i = 0; i < previousGuesses.length; i += 1) {//Start of for loop
+      const currentGuess = previousGuesses[i];
+      if (currentGuess == randomNumber) {
+        guessHistory.innerHTML += `<li class='element' id='background-style-green'>${previousGuesses[i] + 1}</li>`;
+      } else {
+        guessHistory.innerHTML += `<li class='element' id='background-style-red'>${previousGuesses[i] + 1}</li>`;
+      }
+   guessHistory.innerHTML += `</ul>`;
+  }//End of for loop
+}
 
 function disableGridItems() {
   for (let i = 0; i < gridItem.length; i += 1) {
@@ -23,6 +37,8 @@ for (let i = 0; i < gridItem.length; i += 1) {
         indicatorMessage.style.color = "green";
         div.style.backgroundColor = "green";
         div.style.color = "white";
+        previousGuesses.push(i);
+        guessListHistory();
         disableGridItems();
     } else if (previousGuesses.indexOf(i) > -1) {
         gridItem[i].style.pointerEvents = "none"
@@ -32,6 +48,8 @@ for (let i = 0; i < gridItem.length; i += 1) {
         indicatorMessage.style.color = "red";
         div.style.backgroundColor = "red";
         div.style.color = "white";
+        previousGuesses.push(i);
+        guessListHistory();
         disableGridItems();
     } else if (i > randomNumber) {
         div = this;
@@ -41,6 +59,7 @@ for (let i = 0; i < gridItem.length; i += 1) {
         div.style.color = "white";
         previousGuesses.push(i);
         guessCount = previousGuesses.length;
+        guessListHistory();
     } else if (i < randomNumber) {
         div = this;
         indicatorMessage.textContent = `Guess > Higher`;
@@ -49,6 +68,7 @@ for (let i = 0; i < gridItem.length; i += 1) {
         div.style.color = "white";
         previousGuesses.push(i);
         guessCount = previousGuesses.length;
+        guessListHistory();
     }
  });//End of gridItem function
 }//End of for loop
